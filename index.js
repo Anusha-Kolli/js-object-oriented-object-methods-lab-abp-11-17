316f5c22b121634ccecb75d69f7d963d4b218cf6
Naga Anusha Kolli <kolli.anusha9@gmail.com>
2018-01-24 21:24:35 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,8 @@
+function BoardMember (name, homeState, training){
+  this.name = name;
+  this.homeState = homeState;
+  this.training = training;
+  this.sayHello = function(){
+    console.log(`Hello everybody, my name is ${this.name}!`);
+  }
+}
